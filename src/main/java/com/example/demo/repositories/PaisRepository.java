0f0e83c424b6bfd59d451f3418b310a3893a20e6
Luis <ldelacua@gmail.com>
2022-02-25 10:43:43 +0100

    delete
    delete

--- src/main/java/com/example/demo/repositories/PaisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.models.PaisModel;;
-@Repository
-public interface PaisRepository extends JpaRepository<PaisModel,Long>{
-}
--- src/main/java/com/example/demo/services/impl/UserExceptionDataFound.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.services.impl;
-
-public class UserExceptionDataFound extends Throwable {
-
-	public UserExceptionDataFound(String string) {
-		super(string);
-		// TODO Auto-generated constructor stub
-	}
-}
